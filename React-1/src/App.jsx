2948ce559f82108126d7e4d267d8061933048df7
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-06-07 17:54:38 +0530

    I learn what is Dependency Array in useEffect,  What is it's use . to be honest it's little conffusing.

--- React-1/src/App.jsx
+++ React-1/src/App.jsx
@@ -1,64 +1,74 @@
 import { useState, useEffect } from "react"
+import Dependency from "./Dependency"
 
 function App(){
 
   // ----------Conditional Rendering ----------------
-  const [CountVisible , setCountVisible] = useState(true);
-  useEffect(()=>{
-  setInterval(()=>{
-    setCountVisible(c=>!c)
-  },5000)
-  },[])
- // ------------------------------------------------
+//   const [CountVisible , setCountVisible] = useState(true);
+//   useEffect(()=>{
+//   setInterval(()=>{
+//     setCountVisible(c=>!c)
+//   },5000)
+//   },[])
+//  // ------------------------------------------------
 
-  return <div>
-    <b>
-    hi there 
-    </b>
-    {CountVisible ? <Counter></Counter>  :null }
-    {/* <Counter></Counter> */}
-  </div>
-}
+//   return <div>
+//     <b>
+//     hi there 
+//     </b>
+//     {CountVisible ? <Counter></Counter>  :null }
+//     {/* <Counter></Counter> */}
+//   </div>
+// }
 
 
-function Counter(){
+// function Counter(){
+// //  console.log("Counter")
+//   const [count , setCount] = useState(0);
 
-  const [count , setCount] = useState(0);
+//   // function IncreaseCount(){
+//   //   setCount(count +1);
+//   // }
+//   // function resetCount(){
+//   //   setCount(0);
+//   // }
 
-  // function IncreaseCount(){
-  //   setCount(count +1);
-  // }
-  // function resetCount(){
-  //   setCount(0);
-  // }
-
-  // function DecreaseCount(){
-  //   if(count >0){
-  //     setCount(count - 1)
-  //   }
-  // }
+//   // function DecreaseCount(){
+//   //   if(count >0){
+//   //     setCount(count - 1)
+//   //   }
+//   // }
   
 
-  //------------------------make a clock -----------------------//
+//   //------------------------make a clock -----------------------//
 
-  // setInterval(function (){
-  //   setCount(count +1)   // what happenes here after each time "count" state is called the "Counter function" has been called result more "setInterval" called 
-  // }, 1000)              // SO to stop this we have to "useEffect hooks"
-     console.log("Counter")
+//   // setInterval(function (){
+//   //   setCount(count +1)   // what happenes here after each time "count" state is called the "Counter function" has been called result more "setInterval" called 
+//   // }, 1000)              // SO to stop this we have to "useEffect hooks"
+    
 
-  useEffect(function(){  // hooking into the lifecycle event of react 
-    setInterval(function(){
-      setCount(count => count + 1)
-    }, 1000);
-    console.log("Mounted")
-  } , []);  //this "ARRAY" is called dependency ARRAY
+//   useEffect(function(){  // hooking into the lifecycle event of react
+//     console.log("Mounted")
+//     let clock = setInterval(function(){
+//     console.log("from inside setInterval");
 
-  return <div>
-    <h1>{count}</h1>
-    {/* <button onClick={IncreaseCount} >Increase count</button>
-    <button onClick={DecreaseCount}>Decrease count</button>
-    <button onClick={resetCount}>Reset count</button> */}
-  </div>
+//       setCount(count => count + 1)
+//     }, 1000);
+
+//     return function(){
+//       console.log("this call when the component is unMount");
+//       clearInterval(clock)
+//     }
+//   } , []);  //this "ARRAY" is called dependency ARRAY
+
+//   return <div>
+//     <h1>{count}</h1>
+//     {/* <button onClick={IncreaseCount} >Increase count</button>
+//     <button onClick={DecreaseCount}>Decrease count</button>
+//     <button onClick={resetCount}>Reset count</button> */}
+//   </div>
+
+<Dependency/>
 }
 
 export default App
---
+++ React-1/src/Dependency.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Re-Learning cleanup, useDffect, Learning about dependency array
+const Dependency = () => {
+  return (
+    <div>
+      hi thereee
+    </div>
+  )
+}
+
+export default Dependency
