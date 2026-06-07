34daba9ce0f4bc00e59558754e940595932e601e
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-06-07 14:06:41 +0530

    learn component rerandering.

--- React-1/src/App.jsx
+++ React-1/src/App.jsx
@@ -14,21 +14,32 @@ function Counter(){
 
   const [count , setCount] = useState(0);
 
-  function IncreaseCount(){
-    setCount(count +1);
-  }
+  // function IncreaseCount(){
+  //   setCount(count +1);
+  // }
+  // function resetCount(){
+  //   setCount(0);
+  // }
 
-  function DecreaseCount(){
-    if(count >0){
-      setCount(count - 1)
-    }
-  }
+  // function DecreaseCount(){
+  //   if(count >0){
+  //     setCount(count - 1)
+  //   }
+  // }
+  
+
+  //------------------------make a clock -----------------------//
+
+  setInterval(function (){
+    setCount(count +1)
+  }, 1000)
 
 
   return <div>
     <h1>{count}</h1>
-    <button onClick={IncreaseCount} >Increase count</button>
+    {/* <button onClick={IncreaseCount} >Increase count</button>
     <button onClick={DecreaseCount}>Decrease count</button>
+    <button onClick={resetCount}>Reset count</button> */}
   </div>
 }
 
