c3543c04d11690faed5513bb127c229d2918c39d
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-06-23 15:08:59 +0530

    right now I learn about useState hook.

--- React-1/src/App.jsx
+++ React-1/src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  let {count ,setCount} = useState(0);
+  let [count ,setCount] = useState(0);
   function increaseCount(){
-    setCount(count++)
+    setCount(count+1)
   }
   return (
     <div>
