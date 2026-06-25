e17dcfdc6082b059ca3f38ed0e2acb973d2dd3fe
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-06-25 13:57:03 +0530

    lerinig useState hook.

--- React-1/src/App.jsx
+++ React-1/src/App.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  let [count ,setCount] = useState(0);
-  function increaseCount(){
-    setCount(count+1)
-  }
+  
   return (
-    <div>
-       <button onClick={increaseCount}> Count: {count} </button>
-      
-    </div>
+    <>
+   
+
+
+    </>
   )
 }
 function Button(props){
