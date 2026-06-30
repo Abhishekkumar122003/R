949a6aa609639f40af27862ca2cf515b3f787e62
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-06-30 19:38:07 +0530

    learning about component. and this time  i am completing the react .

--- Lec-3/src/App.jsx
+++ Lec-3/src/App.jsx
@@ -5,7 +5,11 @@ const App = () => {
     <div>
       hii  theree
     </div>
+    
   )
 }
+
+    // React give you the ability to structure my code in better way
+    //Defining the state of my application
 
 export default App
