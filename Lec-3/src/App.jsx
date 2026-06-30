5670f6f0a8b6dd9861e17e14fb7d713dba0c61e5
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-06-30 19:49:05 +0530

    trying to implement the linkdin post component.

--- Lec-3/src/App.jsx
+++ Lec-3/src/App.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-
+// React give you the ability to structure my code in better way
+    //Defining the state of my application
 const App = () => {
   return (
     <div>
       hii  theree
+      <PostComponent style={{height:"100vh"}} />
     </div>
     
   )
 }
 
-    // React give you the ability to structure my code in better way
-    //Defining the state of my application
+function PostComponent(){
+  return <div style={{display: "flex" ,width:"100vh", height:"100vh",  borderWidth:1, backgroundColor: "#636e72" , }}>
+    <img src="https://harkirat.classx.co.in/new-courses"  style={{height: 20 , width:20,}}/>
+  </div>
+}
+    
 
 export default App
