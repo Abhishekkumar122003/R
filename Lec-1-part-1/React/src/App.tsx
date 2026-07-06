b24dc7de8603f25d22e3e27ac07e87848353ab13
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-07-06 13:28:00 +0530

    right now i learned thet if I want to pass the props in React component using  tyoescripts, first I have to creat/declear the PostProps object and the pass it to the component as =>  Post(props:PostProps)

--- Lec-1-part-1/React/src/App.tsx
+++ Lec-1-part-1/React/src/App.tsx
@@ -1,4 +1,7 @@
-
+type PostProps = {
+  name:string,
+  content:string
+}
 
 function App() {
 
@@ -6,14 +9,21 @@ function App() {
   return (
     <>
       <div>
-        <h1>hi there</h1>
-        <b>kesee hoo!</b>
-        <button>submit</button>
-        <br />
-
+       <h1> Linked!!!</h1>
+        <Post name= "hrkirt" content= "today I launch new Course" />
+        <Post name="Abhishek" content= "today I learned new thing " />
+        <Post name="shumesh" content= "hi" />
       </div>
     </>
   )
+}
+function Post(props:PostProps) {
+  return   (<>
+        <div style={{marginTop:2, borderRadius:20, padding:20, width:"500px",border:"2px solid black" , borderColor:"black" }}>
+      <div> {props.name} </div>
+      <div> {props.content}</div>
+      </div>
+    </>)
 }
 
 export default App
