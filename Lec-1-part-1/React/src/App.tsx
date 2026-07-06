593d4576538ec397f6b29d90d9da868b97a1e9a4
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-07-06 12:51:27 +0530

    try to understand the component.

--- Lec-1-part-1/React/src/App.tsx
+++ Lec-1-part-1/React/src/App.tsx
@@ -5,7 +5,13 @@ function App() {
 
   return (
     <>
-      
+      <div>
+        <h1>hi there</h1>
+        <b>kesee hoo!</b>
+        <button>submit</button>
+        <br />
+
+      </div>
     </>
   )
 }
