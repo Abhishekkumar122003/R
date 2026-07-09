9f49415b4b4925ebef8ad7d27f57ce3a560dee6a
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-07-09 16:33:39 +0530

    trying to add abort/return function inside useEffect hook to stop the redered component

--- Lec-2-part-2/my-app/src/App.tsx
+++ Lec-2-part-2/my-app/src/App.tsx
@@ -2,26 +2,35 @@ import axios from 'axios'
 import { useEffect, useState } from 'react';
    
 function App(){
-  let [Data , setData] = useState(0)
+  let [Data , setData] = useState<any[]>([]) // initialise the state veriable with an empty ARRAY
   useEffect(()=>{
      
 axios.get("https://jsonplaceholder.typicode.com/todos/")
     .then(response=> {
-       setData (c=> response.data)
+       setData (response.data)
     })
+
+    return function (){
+      
+    }
     
-  }, [setData])
+  }, [])
     return(
       <div>
-        {JSON.stringify(Data)}
+        {Data.map(todo=><Todo userId={todo.userId} id={todo.id} title={todo.title} completed={todo.completed} /> )}
        </div>
     )
   }
 
 function Todo(props:any) {
   return   (<>
-        <div style={{marginTop:2, borderRadius:20, padding:20, width:"500px",border:"2px solid black" , borderColor:"rgb(150, 191, 228)" }}>
-
+        <div style={{marginTop:2, borderRadius:20, padding:20, width:"500px",border:"2px solid black" , fontSize:7 ,borderColor:"rgb(150, 191, 228)" }}>
+          <h1>{props.userId} </h1>
+          <div>
+            <h3>{props.id} </h3>
+            <p>{props.title} </p>
+            <h2>{props.completed? "completed" : "Not Completed"} </h2>
+          </div>
 
       </div>
     </>)
